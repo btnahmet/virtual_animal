9b3cd33fecc12894c7bb84141456932005fe1daf
btnahmet <ahmetbutun27@gmail.com>
2025-06-21 14:56:16 +0300

    Yorum Satırı Commit

--- lib/screens/animals/animal_model.dart
+++ lib/screens/animals/animal_model.dart
@@ -1,25 +1,3 @@
-// class AnimalModel {
-//   double health;
-//   double happiness;
-//   double hunger;
-
-//   AnimalModel({this.health = 1, this.happiness = 1, this.hunger = 1});
-
-//   void updateStatus(String action) {
-//     if (action == "feed") {
-//       health = (health + 10).clamp(0, 100);
-//       happiness = (happiness + 5).clamp(0, 100);
-//       hunger = (hunger - 15).clamp(0, 100);
-//     } else if (action == "clean") {
-//       health = (health + 8).clamp(0, 100);
-//       happiness = (happiness + 6).clamp(0, 100);
-//     } else if (action == "play") {
-//       health = (health + 5).clamp(0, 100);
-//       happiness = (happiness + 10).clamp(0, 100);
-//       hunger = (hunger + 8).clamp(0, 100);
-//     }
-//   }
-// }
 class AnimalModel {
   int? id;
   String name;
